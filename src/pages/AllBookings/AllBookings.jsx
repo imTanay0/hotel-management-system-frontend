c42ef4edff2b702f027ceb8f6aa9df8cdc6f3be4
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-04-22 15:09:31 +0530

    Create Structure of All Booking Page

--- src/pages/AllBookings/AllBookings.jsx
+++ src/pages/AllBookings/AllBookings.jsx
@@ -12,24 +12,63 @@ const AllBookings = () => {
         <button className='btn'>Book a new customer {`>`}</button>
       </Link>
 
-      <div className='container'>
+      <div className='table-container'>
         <table>
-          <th>Name</th>
-          <th>Booking from</th>
-          <th>Booking to</th>
-          <th>Contact no.</th>
-          <th>Type of Room</th>
-          <th>Rate Negotiated</th>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Booking from</th>
+              <th>Booking to</th>
+              <th>Contact no.</th>
+              <th>Type of Room</th>
+              <th>Rate Negotiated</th>
+            </tr>
 
-          <tr>
-            <td>User 1</td>
-            <td>12-09-2022</td>
-            <td>12-10-2022</td>
-            <td>098394898</td>
-            <td>Family Suite</td>
-            <td>5000</td>
-          </tr>
-          
+            <tr>
+              <td>User Name 1</td>
+              <td>12-09-2022</td>
+              <td>12-10-2022</td>
+              <td>098394898</td>
+              <td>Family Suite</td>
+              <td>5000</td>
+            </tr>
+
+            <tr>
+              <td>User 2</td>
+              <td>12-09-2022</td>
+              <td>12-10-2022</td>
+              <td>098394898</td>
+              <td>Family Suite</td>
+              <td>5090</td>
+            </tr>
+
+            <tr>
+              <td>User 3</td>
+              <td>12-09-2022</td>
+              <td>12-10-2022</td>
+              <td>098394898</td>
+              <td>Family Suite</td>
+              <td>5000</td>
+            </tr>
+
+            <tr>
+              <td>User 4</td>
+              <td>12-09-2022</td>
+              <td>12-10-2022</td>
+              <td>098394898</td>
+              <td>Family Suite</td>
+              <td>5000</td>
+            </tr>
+
+            <tr>
+              <td>User 5</td>
+              <td>12-09-2022</td>
+              <td>12-10-2022</td>
+              <td>098394898</td>
+              <td>Family Suite</td>
+              <td>5000</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </section>
