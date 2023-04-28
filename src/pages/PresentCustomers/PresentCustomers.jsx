7281fb30bdf93d761a59bd1d95b89b2f15b97e9c
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-04-28 21:11:10 +0530

    fix bugs

--- src/pages/PresentCustomers/PresentCustomers.jsx
+++ src/pages/PresentCustomers/PresentCustomers.jsx
@@ -37,26 +37,6 @@ const PresentCustomers = () => {
     getResidents();
   }, [])
 
-  // [{
-  //   GSTIN_no: "1wsdf234",
-  //   address: "Assam",
-  //   booking_from: "2023-04-29T00:00:00.000Z",
-  //   booking_to: "2023-05-15T00:00:00.000Z",
-  //   company_name: "c1",
-  //   date_of_booking: "2023-04-22T00:00:00.000Z",
-  //   date_of_check_in: "2023-05-01T00:00:00.000Z",
-  //   local_contact_number: "4321",
-  //   name: "Sample Customer 1",
-  //   phone_number: "1234",
-  //   rate_negotiated: 1000,
-  //   roomAllocatedStatus: true,
-  //   room_no: { no: '101', id: '64468bfa6e6a961f41089b60' },
-  //   room_type: { name: 'Type 1', id: '64467c2f0e7ff6d5f79d66d4' },
-  //   user_foods: [],
-  //   __v: 0,
-  //   _id: "64469da085ae0ca1b27bda93"
-  // }]
-
   function calculateDays(date1, date2) {
     const newDate1 = new Date(date1);
     const newDate2 = new Date(date2);
