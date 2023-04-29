d1d9fa4ce1133bbd320c0f52f4241373f7c9cc98
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-04-29 19:27:12 +0530

    update all bookings page

--- src/pages/AllBookings/AllBookings.jsx
+++ src/pages/AllBookings/AllBookings.jsx
@@ -1,65 +1,76 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './allBookings.css';
 
-
 const AllBookings = () => {
-
   const [bookedCustomers, setBookedCustomers] = useState([]);
+  const [availableRooms, setAvailableRooms] = useState({});
 
   useEffect(() => {
-
-    const getBookedCustomers = async () => {
-
+    const fetchBookedCustomers = async () => {
       try {
         const response = await fetch('http://localhost:8080/api/v1/user/bookings', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-          }
-        })
+          },
+        });
+
         const result = await response.json();
 
         if (response.ok) {
+          const { users, availableRoomsByUser } = result;
 
-          const data = result.users;
+          const formattedCustomers = users.map(customer => {
+            const {
+              booking_from,
+              booking_to,
+              rate_negotiated,
+              room_type: { name },
+              name: customerName,
+              phone_number,
+              availableRooms,
+            } = customer;
 
-          for (let i = 0; i < data.length; i++) {
-            data[i].booking_from = new Date(data[i].booking_from).toLocaleDateString();
-            data[i].booking_to = new Date(data[i].booking_to).toLocaleDateString();
-            data[i].rate_negotiated = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'INR' }).format(data[i].rate_negotiated);
-            data[i].room_type.name = data[i].room_type.name.charAt(0).toUpperCase() + data[i].room_type.name.slice(1);
-            data[i].name = data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1);
-            data[i].phone_number = data[i].phone_number.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
-          }
+            return {
+              booking_from: new Date(booking_from).toLocaleDateString(),
+              booking_to: new Date(booking_to).toLocaleDateString(),
+              rate_negotiated: new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'INR',
+              }).format(rate_negotiated),
+              room_type: name.charAt(0).toUpperCase() + name.slice(1),
+              name: customerName.charAt(0).toUpperCase() + customerName.slice(1),
+              phone_number: phone_number.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3'),
+              availableRooms,
+            };
+          });
 
-          setBookedCustomers(data);
+          setBookedCustomers(formattedCustomers);
+          setAvailableRooms(availableRoomsByUser);
         } else {
           throw new Error(`Failed to fetch data: ${result.message}`);
         }
-
       } catch (error) {
         alert(error.message);
       }
-    }
+    };
 
-    getBookedCustomers();
-
-  }, [])
-
+    fetchBookedCustomers();
+  }, []);
 
   return (
     <section className='all-booking__section'>
       <h1>All Bookings</h1>
 
-      <Link to="/bookings/add">
-        <button className='btn'>Book a new customer {`>`}</button>
+      <Link to='/bookings/add'>
+        <button className='btn'>Book a new customer {'>'}</button>
       </Link>
 
       <div className='table-container'>
         <table>
-          <tbody>
+          <thead>
             <tr>
               <th>Name</th>
               <th>Booking from</th>
@@ -67,21 +78,29 @@ const AllBookings = () => {
               <th>Contact no.</th>
               <th>Type of room</th>
               <th>Rate negotiated</th>
-              <th>Room availability</th>
+              <th>Rooms available</th>
               <th>Book room</th>
             </tr>
-
-            {bookedCustomers.map((a) => (
-              <tr key={a.name}>
-                <td>{a.name}</td>
-                <td>{a.booking_from}</td>
-                <td>{a.booking_to}</td>
-                <td>{a.phone_number}</td>
-                <td>{a.room_type.name}</td>
-                <td>{a.rate_negotiated}</td>
-                <td>Available</td>
+          </thead>
+          <tbody>
+            {bookedCustomers.map(customer => (
+              <tr key={customer.name}>
+                <td>{customer.name}</td>
+                <td>{customer.booking_from}</td>
+                <td>{customer.booking_to}</td>
+                <td>{customer.phone_number}</td>
+                <td>{customer.room_type}</td>
+                <td>{customer.rate_negotiated}</td>
                 <td>
-                  <Link to="/allocate-room"><button className='btn-light'>Book Now</button></Link>
+                  {availableRooms[customer.name] &&
+                    availableRooms[customer.name].map(room => (
+                      <span key={room}>{room} </span>
+                    ))}
+                </td>
+                <td>
+                  <Link to='/allocate-room'>
+                    <button className='btn-light'>Book Now</button>
+                  </Link>
                 </td>
               </tr>
             ))}
@@ -89,7 +108,7 @@ const AllBookings = () => {
         </table>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AllBookings
+export default AllBookings;
