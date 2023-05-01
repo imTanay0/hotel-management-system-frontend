cd47cc69cdab145f7ae5dd71dffd36d3abab639b
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-05-02 00:34:09 +0530

    Changing billing page layout

--- src/pages/Billing/Billing.jsx
+++ src/pages/Billing/Billing.jsx
@@ -8,56 +8,9 @@ const Billing = () => {
     <div className='billing-section'>
       <h1>Billing</h1>
 
-      <div className='billing-section-form'>
-        <form>
-          <label>Name of the Customer: <input type="text" /></label>
+      <div>
+        <span>Date Of check in: 12/09/2023</span>
 
-          <br />
-          <br />
-
-          <label>Photo Identity: <input type="text" /></label>
-
-          <br />
-          <br />
-
-          <label>Phone no: <input type="text" /></label>
-
-          <br />
-          <br />
-
-          <label>Address: <input type="text" /></label>
-
-          <br />
-          <br />
-
-          <label>Room No: <input type="text" /></label>
-
-          <br />
-          <br />
-
-          <label>Local Contact No: <input type="text" /></label>
-
-          <br />
-          <br />
-
-          <label>Date of Check in: <input type="date" /></label>
-
-          <br />
-          <br />
-
-          <label>Company Name: <input type="text" /></label>
-
-          <br />
-          <br />
-
-          <label>GSTIN No: <input type="text" /></label>
-
-          <br />
-          <br />
-
-          <button className='btn' type='submit'>Submit</button>
-
-        </form>
       </div>
     </div>
   )
