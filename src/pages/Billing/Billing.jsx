6905fe1fef423ccea2901dc8c25eb164351fccba
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-05-10 20:35:44 +0530

    Create billing page structure

--- src/pages/Billing/Billing.jsx
+++ src/pages/Billing/Billing.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import './billing.css'
 
@@ -8,10 +7,45 @@ const Billing = () => {
     <div className='billing-section'>
       <h1>Billing</h1>
 
-      <div>
-        <span>Date Of check in: 12/09/2023</span>
-
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date Of check in</th>
+              <th>Date Of check out</th>
+              <th>GSTIN no</th>
+              <th>Days spent</th>
+              <th>Room rent</th>
+              <th>Room bill</th>
+              <th>Food bill</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>12/09/2023</td>
+              <td>22/10/2023</td>
+              <td>gstin</td>
+              <td>7 days</td>
+              <td>6000</td>
+              <td>10000</td>
+              <td>1200</td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>12/09/2023</td>
+              <td>22/10/2023</td>
+              <td>gstin</td>
+              <td>7 days</td>
+              <td>6000</td>
+              <td>10000</td>
+              <td>1200</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+      
     </div>
   )
 }
